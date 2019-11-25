93bd8dc0161cd38231cca90c78ce474ec6df9d6c
Zoltán <zoltanfraknoy@gmail.com>
2019-11-25 13:37:13 +0100

    implements Read method

--- src/com/company/Reader.java
+++ src/com/company/Reader.java
@@ -1,9 +1,25 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Reader {
 
 
-    public static void read(){
-        
+    public static ArrayList<Person> read() throws FileNotFoundException {
+        ArrayList<Person> peopleList = new ArrayList<>();
+        Scanner sc = new Scanner(new File("src/us-500.csv"));
+        while (sc.hasNext()) {
+            String line = sc.nextLine();
+            String[] fields = line.split(",");
+            String fullName = fields[0] + fields[1];
+            String city = fields[2];
+            String email = fields[3];
+            Person person = new Person(fullName,city,email);
+            peopleList.add(person);
+        }
+        return peopleList;
     }
 }
