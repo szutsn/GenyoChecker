c88912fc22bb87a056bf99e91ac07d65e776830e
Zoltán <zoltanfraknoy@gmail.com>
2019-11-25 14:30:07 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/com/company/Reader.java
+++ src/com/company/Reader.java
@@ -14,6 +14,7 @@ public class Reader {
 
         ArrayList<Person> peopleList = new ArrayList<>();
         Scanner sc = new Scanner(new File("src/us-500.csv"));
+        ArrayList<Person> peopleList = new ArrayList<>();
         while (sc.hasNext()) {
             String line = sc.nextLine();
             String[] fields = line.split(",");
