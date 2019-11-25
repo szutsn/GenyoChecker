8973cd740d9d72f5ca2f9ae1d62c0827760904ee
Sz N <norbcore@gmail.com>
2019-11-25 14:57:40 +0100

    javítva genyó

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,10 +18,12 @@ public class Main {
 
             System.out.println(list.get(i).getFullName());
         }*/
-        for (int i = 0; i < list.size()-1; i++) {
-
-            System.out.println(Filter.cityFilter("Chicago", list).get(i).getEmail());
+        ArrayList<Person> chicagoPeople = Filter.cityFilter("Chicago", list);
+        for (Person chicagoPerson : chicagoPeople) {
+            System.out.println(chicagoPerson.getEmail());
         }
+
+
 
     }
 }
